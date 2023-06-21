8013b477a6939404cbf8726b7f328b61a7971ae2
SAFWAT <safwathkhan2@gmail.com>
2023-06-22 02:20:22 +0530

    Images Removed from Admin

--- register.php
+++ register.php
@@ -57,31 +57,31 @@ if (isset($_POST['submit'])) {
                 //Create an instance; passing `true` enables exceptions
                 $mail = new PHPMailer(true);
 
-                try {
-                    //Server settings
-                    $mail->SMTPDebug = SMTP::DEBUG_SERVER; //Enable verbose debug output
-                    $mail->isSMTP(); //Send using SMTP
-                    $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
-                    $mail->SMTPAuth = true; //Enable SMTP authentication
-                    $mail->Username = 'kushicreamparlour@gmail.com'; //SMTP username
-                    $mail->Password = 'ztrxmczvfohrmisc'; //SMTP password
-                    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
-                    $mail->Port = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+                // try {
+                //     //Server settings
+                //     $mail->SMTPDebug = SMTP::DEBUG_SERVER; //Enable verbose debug output
+                //     $mail->isSMTP(); //Send using SMTP
+                //     $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
+                //     $mail->SMTPAuth = true; //Enable SMTP authentication
+                //     $mail->Username = 'kushicreamparlour@gmail.com'; //SMTP username
+                //     $mail->Password = 'ztrxmczvfohrmisc'; //SMTP password
+                //     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; //Enable implicit TLS encryption
+                //     $mail->Port = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-                    //Recipients
-                    $mail->setFrom('kushicreamparlour@gmail.com');
-                    $mail->addAddress($email);
+                //     //Recipients
+                //     $mail->setFrom('kushicreamparlour@gmail.com');
+                //     $mail->addAddress($email);
 
-                    //Content
-                    $mail->isHTML(true); //Set email format to HTML
-                    $mail->Subject = 'no reply';
-                    $mail->Body = 'Here is the verification link <b><a href="http://localhost/baker/login.php?verification=' . $code . '">Verify here</a></b>';
+                //     //Content
+                //     $mail->isHTML(true); //Set email format to HTML
+                //     $mail->Subject = 'no reply';
+                //     $mail->Body = 'Here is the verification link <b><a href="http://localhost/baker/login.php?verification=' . $code . '">Verify here</a></b>';
 
-                    $mail->send();
-                    echo 'Message has been sent';
-                } catch (Exception $e) {
-                    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-                }
+                //     $mail->send();
+                //     echo 'Message has been sent';
+                // } catch (Exception $e) {
+                //     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+                // }
                 echo "</div>";
                 msg("We've send a verification link on your email address.", "info");
             } else {
